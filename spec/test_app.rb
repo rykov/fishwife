9709a6ef9e89fac703cb62c8fa0b312c809195b1
David Kellum <dek-oss@gravitext.com>
2011-08-18 21:18:12 -0700

    Whitespace cleanup; 2-space indent

--- spec/test_app.rb
+++ spec/test_app.rb
@@ -1,6 +1,6 @@
-#
-# A tiny Rack application for testing the Mizuno webserver.  Each of the
-# following paths can be used to test webserver behavior:
+
+# A tiny Rack application for testing the Mizuno webserver.  Each of
+# the following paths can be used to test webserver behavior:
 #
 # /ping:: Always returns 200 OK.
 #
@@ -15,85 +15,86 @@
 # /pull:: Recieves messages sent via /push using async.
 #
 # A request to any endpoint not listed above will return a 404 error.
-#
 class TestApp
-    def initialize
-        @subscribers = Array.new
-    end
+  def initialize
+    @subscribers = Array.new
+  end
 
-    def call(env)
-        begin
-            request = Rack::Request.new(env)
-            method = request.path[/^\/(\w+)/, 1]
-            return(error(request, 404)) if (method.nil? or method.empty?)
-            return(error(request, 404)) unless respond_to?(method.to_sym)
-            send(method.to_sym, request)
-        rescue => error
-            puts error
-            puts error.backtrace
-            error(nil, 500)
-        end
+  def call(env)
+    begin
+      request = Rack::Request.new(env)
+      method = request.path[/^\/(\w+)/, 1]
+      return(error(request, 404)) if (method.nil? or method.empty?)
+      return(error(request, 404)) unless respond_to?(method.to_sym)
+      send(method.to_sym, request)
+    rescue => error
+      puts error
+      puts error.backtrace
+      error(nil, 500)
     end
+  end
 
-    def ping(request)
-        [ 200, { "Content-Type" => "text/plain", 
-            "Content-Length" => "2" }, [ "OK" ] ]
-    end
+  def ping(request)
+    [ 200, { "Content-Type" => "text/plain",
+        "Content-Length" => "2" }, [ "OK" ] ]
+  end
 
-    def error(request, code = nil)
-        code ||= (request.path[/^\/\w+\/(\d+)/, 1] or "500")
-        [ code.to_i, { "Content-Type" => "text/plain", 
-            "Content-Length" => "5" }, [ "ERROR" ] ]
-    end
+  def error(request, code = nil)
+    code ||= (request.path[/^\/\w+\/(\d+)/, 1] or "500")
+    [ code.to_i,
+      { "Content-Type" => "text/plain",
+        "Content-Length" => "5" },
+      [ "ERROR" ] ]
+  end
 
-    def echo(request)
+  def echo(request)
+    response = Rack::Response.new
+    env = request.env.merge('request.params' => request.params)
+    response.write(env.to_yaml)
+    response.finish
+  end
+
+  def push(request)
+    message = request.params['message']
+
+    @subscribers.reject! do |subscriber|
+      begin
         response = Rack::Response.new
-        env = request.env.merge('request.params' => request.params)
-        response.write(env.to_yaml)
-        response.finish
-    end
-
-    def push(request)
-        message = request.params['message']
-
-        @subscribers.reject! do |subscriber|
-            begin
-                response = Rack::Response.new
-                if(message.empty?)
-                    subscriber.call(response.finish)
-                    next(true)
-                else
-                    response.write(message)
-                    subscriber.call(response.finish)
-                    next(false)
-                end
-            rescue java.io.IOException => error
-                next(true)
-            end
+        if(message.empty?)
+          subscriber.call(response.finish)
+          next(true)
+        else
+          response.write(message)
+          subscriber.call(response.finish)
+          next(false)
         end
-
-        ping(request)
+      rescue java.io.IOException => error
+        next(true)
+      end
     end
 
-    def pull(request)
-        @subscribers << request.env['async.callback']
-        throw(:async)
-    end
+    ping(request)
+  end
 
-    def download(request)
-        file = File.new('spec/data/reddit-icon.png', 'r')
-        response = Rack::Response.new(file)
-        response['Content-Type'] = 'image/png'
-        response['Content-Disposition'] = \
-            'attachment; filename=reddit-icon.png'
-        response.finish
-    end
+  def pull(request)
+    @subscribers << request.env['async.callback']
+    throw(:async)
+  end
 
-    def upload(request)
-        data = request.params['file'][:tempfile].read
-        checksum = Digest::MD5.hexdigest(Base64.decode64(data))
-        response = Rack::Response.new
-        response.write(checksum)
-        response.finish
-    end
+  def download(request)
+    file = File.new('spec/data/reddit-icon.png', 'r')
+    response = Rack::Response.new(file)
+    response['Content-Type'] = 'image/png'
+    response['Content-Disposition'] =
+      'attachment; filename=reddit-icon.png'
+    response.finish
+  end
+
+  def upload(request)
+    data = request.params['file'][:tempfile].read
+    checksum = Digest::MD5.hexdigest(Base64.decode64(data))
+    response = Rack::Response.new
+    response.write(checksum)
+    response.finish
+  end
 end
