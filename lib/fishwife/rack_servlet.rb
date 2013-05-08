66547a1266b3a2089da8d6ed7ce5290a956a3d7d
David Kellum <dek-oss@gravitext.com>
2013-05-07 17:14:20 -0700

    2013

--- lib/fishwife/rack_servlet.rb
+++ lib/fishwife/rack_servlet.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2011-2012 David Kellum
+# Copyright (c) 2011-2013 David Kellum
 # Copyright (c) 2010-2011 Don Werve
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you
